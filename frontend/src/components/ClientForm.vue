e8a7090d24418cd7d1fb98e5431aff84be56ffc0
leeansilva <leeansilva@gmail.com>
2024-12-07 00:12:19 -0300

    ClientForm

---
+++ frontend/src/components/ClientForm.vue
@@ -0,0 +1,44 @@
+<template>
+    <q-dialog v-model="open">
+      <q-card>
+        <q-card-section>
+          <div class="text-h6">{{ isEdit ? 'Editar Cliente' : 'Agregar Cliente' }}</div>
+        </q-card-section>
+  
+        <q-card-section>
+          <q-input v-model="form.name" label="Nombre" />
+          <q-input v-model="form.dni" label="DNI" />
+          <q-input v-model="form.address" label="Dirección" />
+        </q-card-section>
+  
+        <q-card-actions align="right">
+          <q-btn flat label="Cancelar" @click="close" />
+          <q-btn flat label="Guardar" @click="save" />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+  </template>
+  
+  <script>
+  import { useClientContext } from '../context/ClientContext'
+  
+  export default {
+    props: { open: Boolean, client: Object },
+    setup(props, { emit }) {
+      const { addClient, editClient } = useClientContext()
+      const isEdit = !!props.client
+      const form = ref({ ...props.client })
+  
+      const save = () => {
+        if (isEdit) editClient(props.clientIndex, form.value)
+        else addClient(form.value)
+        close()
+      }
+  
+      const close = () => emit('close')
+  
+      return { form, isEdit, save, close }
+    }
+  }
+  </script>
+  
