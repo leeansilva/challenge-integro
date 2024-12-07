1be8f2f763284ab5f7c09a8962af39f498b5e529
leeansilva <leeansilva@gmail.com>
2024-12-07 00:12:37 -0300

    HomePage

---
+++ frontend/src/pages/HomePage.vue
@@ -0,0 +1,21 @@
+<template>
+    <main-layout>
+      <Header @openForm="showForm = true" />
+      <Table @editClient="editClient" />
+      <ClientForm v-if="showForm" :open="showForm" @close="showForm = false" />
+    </main-layout>
+  </template>
+  
+  <script>
+  import { provideClientContext } from '../context/ClientContext'
+  
+  export default {
+    setup() {
+      provideClientContext()
+      const showForm = ref(false)
+  
+      return { showForm }
+    }
+  }
+  </script>
+  
